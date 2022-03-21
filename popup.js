b30cfa77556e8e7fcb60696db7aceb2677935f4e
Adan Vasconcelos <goadan@gmail.com>
2022-03-21 10:01:29 -0400

    checked if user is already saved

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-const saveUserId = document.getElementById('save');
+const btn_save_user_id = document.getElementById('save');
 var saved_userid = "";
-var input_userid = document.getElementById('userid');
+var txt_userid = document.getElementById('userid');
 
-saveUserId.addEventListener('click', () => {
+btn_save_user_id.addEventListener('click', () => {
   var new_userid = document.getElementById('userid').value;
   
   //alert('New userid is ' + new_userid);
@@ -15,7 +15,14 @@ saveUserId.addEventListener('click', () => {
 
 chrome.storage.local.get(['userid'], function(result) {
   saved_userid = result.userid;
-  input_userid.value = saved_userid;
+
+  if (saved_userid != null) {
+    txt_userid.value = saved_userid;
+  }
+  else {
+    txt_userid.value = "";
+  }
+  
   console.log('Stored userid is ' + saved_userid);
   //alert('Stored userid is ' + saved_userid);
 });
