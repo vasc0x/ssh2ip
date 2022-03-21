17c9566ad05681a2728f310cc4cf0b9367d58b44
Adan Vasconcelos <goadan@gmail.com>
2022-03-21 16:12:33 -0400

    fixed menu id errors by moving the menu creation inside the onInstalled listener

--- background.js
+++ background.js
@@ -1,25 +1,37 @@
-// chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
+	var contextMenuItem = {
+		"id": "ssh", 
+		"title": "SSH to selected IP",
+		"contexts": ["selection"]
+	}
 
-// });
-
-var contextMenuItem = {
-	"id": "ssh", 
-	"title": "SSH to selected IP",
-	"contexts": ["selection"]
-}
+	// Remove all ids before creating new one
+	chrome.contextMenus.removeAll(function() {
+		chrome.contextMenus.create(contextMenuItem);
+	});
+});
 
 var saved_userid = "";
-
-chrome.contextMenus.create(contextMenuItem);
+var newURL = "";
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	chrome.storage.local.get(['userid'], function(result) {
 		saved_userid = result.userid;
-		console.log('Value currently is ' + saved_userid);
 
-		var newURL = "ssh://" + saved_userid + "@" + info.selectionText;
-		console.log(newURL);
-		chrome.tabs.create({url: newURL})
+		// Check if we have a user id saved already
+		if (saved_userid != null) {
+			console.log('Value currently is ' + saved_userid);
+
+			newURL = "ssh://" + saved_userid + "@" + info.selectionText;
+			console.log(newURL);
+			chrome.tabs.create({url: newURL})
+		}
+		else {
+			console.log('There is no user id stored');
+
+			newURL = "ssh://" + info.selectionText;
+			console.log(newURL);
+			chrome.tabs.create({url: newURL})
+		}
 	});
 });
-
