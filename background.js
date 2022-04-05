da9e3a469a85d5c9fb3fdc2bc3531df26fa12a46
vasc0x <avasc007@fiu.edu>
2022-04-05 09:51:42 -0400

    add password field

--- background.js
+++ background.js
@@ -9,30 +9,68 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.contextMenus.removeAll(function() {
 		chrome.contextMenus.create(contextMenuItem);
 	});
+
+
 });
 
+// let page = document.body.innerHTML;
+// //let ip = page.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g);
+// page = page.replace(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g, 'hi');
+// alert (page);
+
+// chrome.runtime.onMessage.addListener(function(request, sender) {
+// 	if (request.action == "getSource") {
+// 		this.pageSource = request.source;
+// 		var title = this.pageSource.match(/<title[^>]*>([^<]+)<\/title>/)[1];
+// 		alert(title)
+// 	}
+// });
+
+// chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+// 	chrome.tabs.executeScript(
+// 		tabs[0].id,
+// 		{ code: 'var s = document.documentElement.outerHTML; chrome.runtime.sendMessage({action: "getSource", source: s});' }
+// 	);
+// });
+
+// const CryptoJS = require('crypto-js');
+
 var saved_userid = "";
+var saved_password = "";
+var saved_ssh_client = "";
 var newURL = "";
 
-// Add listener to contextual menu
+/* Add listener to contextual menu */
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
+	/* Get saved user_id */
 	chrome.storage.local.get(['userid'], function(result) {
 		saved_userid = result.userid;
-
-		// Check if we have a user id saved already
-		if (saved_userid != null) {
-			console.log('Value currently is ' + saved_userid);
-
-			newURL = "ssh://" + saved_userid + "@" + info.selectionText;
-			console.log(newURL);
-			chrome.tabs.create({url: newURL})
-		}
-		else {
-			console.log('There is no user id stored');
-
-			newURL = "ssh://" + info.selectionText;
-			console.log(newURL);
-			chrome.tabs.create({url: newURL})
-		}
 	});
+
+	/* Get saved password */
+	chrome.storage.local.get(['password'], function(result) {
+		saved_password = result.password;
+	});
+
+	/* Get saved ssh_client */
+	chrome.storage.local.get(['ssh_client'], function(result) {
+		saved_userid = result.userid;
+	});
+
+	/* Check if we have a user id saved already */
+	if (saved_userid != null) {
+		console.log('Value currently is ' + saved_userid);
+
+		newURL = "ssh://" + saved_userid + "@" + info.selectionText;
+		console.log(newURL);
+		chrome.tabs.create({url: newURL})
+	}
+	else {
+		console.log('There is no user id stored');
+
+		newURL = "ssh://" + info.selectionText;
+		console.log(newURL);
+		chrome.tabs.create({url: newURL})
+	}
+	
 });
--- popup.js
+++ popup.js
@@ -1,33 +1,36 @@
-// Save user id button
-const btn_save_user_id = document.getElementById('save');
+const btn_save_user_id = document.getElementById('save');               // Save user_id button
 
 var saved_userid = "";
 
-// Input textbox containing the userid
-var txt_userid = document.getElementById('userid');
 
-// Add an event listener to the save button
+var txt_userid = document.getElementById('userid');                     // Input textbox containing the user_id
+var txt_password = document.getElementById('password');                 // Input textbox containing the password
+
+/* Add an event listener to the save button */
 btn_save_user_id.addEventListener('click', () => {
-	// Get the userd id value from the textbox
-  var new_userid = document.getElementById('userid').value;
-  var sync_to_google = document.getElementById('sync').checked;
+  var new_userid = document.getElementById('userid').value;             // Get the userd id value from the textbox
+  var new_password = document.getElementById('password').value;         // Get the password value from the textbox
+  //var new_ssh_client = document.getElementById('ssh_client').value;   // Get the value of the selected ssh client
+  var sync_to_google = document.getElementById('sync').checked;         // Get the sync value from the checkbox
 
-  // If the sync is selected, save to local storage
-  if (sync_to_google) {
-    // Save sync preference to local storage
-    chrome.storage.local.set({'sync': sync_to_google}, function() {
-      console.log('Sync is set to ' + sync_to_google);
+  /* If the user id is set, save to local storage */
+  if (new_userid) {
+    chrome.storage.local.set({'userid': new_userid}, function() {
+      console.log('User id is set to ' + new_userid);
+      window.close();
     });
   }
 
-  // Save the user id to local storage
-  chrome.storage.local.set({'userid': new_userid}, function() {
-    console.log('User id is set to ' + new_userid);
-    window.close();
-  });
+  /* If the password is set, save to local storage */
+  if (new_password) {
+    chrome.storage.local.set({'password': new_password}, function() {
+      console.log('Password was saved to local storage');
+      window.close();
+    });
+  }
 });
 
-// Get the saved user id from local storage
+/* Get the saved user id from local storage */
 chrome.storage.local.get(['userid'], function(result) {
   saved_userid = result.userid;
 
@@ -35,22 +38,36 @@ chrome.storage.local.get(['userid'], function(result) {
     txt_userid.value = saved_userid;
   }
   else {
-    txt_userid.value = "";
+    txt_userid.value = "Your user id";
   }
   
   console.log('Stored userid is ' + saved_userid);
 });
 
-// Check if we are syncing to google local storage
-chrome.storage.local.get(['userid'], function(result) {
-  saved_userid = result.userid;
+/* Get the saved password from local storage */
+chrome.storage.local.get(['password'], function(result) {
+  saved_password = result.password;
 
-  if (saved_userid != null) {
-    txt_userid.value = saved_userid;
+  if (saved_password != null) {
+    txt_password.value = saved_password;
   }
   else {
-    txt_userid.value = "";
+    txt_password.value = "Your password";
   }
   
-  console.log('Stored userid is ' + saved_userid);
+  console.log('Stored password was retrieved');
 });
+
+// // Check if we are syncing to google local storage
+// chrome.storage.local.get(['userid'], function(result) {
+//   saved_userid = result.userid;
+
+//   if (saved_userid != null) {
+//     txt_userid.value = saved_userid;
+//   }
+//   else {
+//     txt_userid.value = "";
+//   }
+  
+//   console.log('Stored userid is ' + saved_userid);
+// });
